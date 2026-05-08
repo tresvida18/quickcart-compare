1a593a4d9b51e9fcbe80b368d4d496f9eeba7c2c
tresvida18 <tresvida18@gmail.com>
2026-05-08 15:41:14 +0530

    Update products.ts

--- components/data/products.ts
+++ components/data/products.ts
@@ -4,19 +4,19 @@ export const products = [
    results: [
      {
        name: "Blinkit",
-       logo: "/blinkit.svg",
+       logo: "/blinkit.png",
        price: 711,
        time: 12,
      },
      {
        name: "Zepto",
-       logo: "/zepto.svg",
+       logo: "/zepto.png",
        price: 718,
        time: 13,
      },
      {
        name: "Instamart",
-       logo: "/instamart.svg",
+       logo: "/instamart.png",
        price: 705,
        time: 19,
      },
@@ -28,19 +28,19 @@ export const products = [
    results: [
      {
        name: "Blinkit",
-       logo: "/blinkit.svg",
+       logo: "/blinkit.png",
        price: 130,
        time: 12,
      },
      {
        name: "Zepto",
-       logo: "/zepto.svg",
+       logo: "/zepto.png",
        price: 131,
        time: 12,
      },
      {
        name: "Instamart",
-       logo: "/instamart.svg",
+       logo: "/instamart.png",
        price: 168,
        time: 14,
      },
@@ -52,19 +52,19 @@ export const products = [
    results: [
      {
        name: "Blinkit",
-       logo: "/blinkit.svg",
+       logo: "/blinkit.png",
        price: 73,
        time: 12,
      },
      {
        name: "Zepto",
-       logo: "/zepto.svg",
+       logo: "/zepto.png",
        price: 75,
        time: 12,
      },
      {
        name: "Instamart",
-       logo: "/instamart.svg",
+       logo: "/instamart.png",
        price: 72,
        time: 14,
      },
@@ -76,19 +76,19 @@ export const products = [
    results: [
      {
        name: "Blinkit",
-       logo: "/blinkit.svg",
+       logo: "/blinkit.png",
        price: 42,
        time: 10,
      },
      {
        name: "Zepto",
-       logo: "/zepto.svg",
+       logo: "/zepto.png",
        price: 45,
        time: 11,
      },
      {
        name: "Instamart",
-       logo: "/instamart.svg",
+       logo: "/instamart.png",
        price: 40,
        time: 15,
      },
