e2d44e252ab3d2ea0ca4a6701470503c56c42421
tresvida18 <tresvida18@gmail.com>
2026-05-09 16:59:11 +0530

    Update products.ts

--- components/data/products.ts
+++ components/data/products.ts
@@ -1,94 +1,85 @@
 export const products = [
- {
-   name: "almond",
-   results: [
-     {
-       name: "Blinkit",
-       logo: "/logos/blinkit.png",
-       price: 711,
-       time: 12,
-     },
-     {
-       name: "Zepto",
-       logo: "/logos/zepto.png",
-       price: 718,
-       time: 13,
-     },
-     {
-       name: "Instamart",
-       logo: "/logos/instamart.png",
-       price: 705,
-       time: 19,
-     },
-   ],
- },
- {
-   name: "apple",
-   results: [
-     {
-       name: "Blinkit",
-       logo: "/logos/blinkit.png",
-       price: 130,
-       time: 12,
-     },
-     {
-       name: "Zepto",
-       logo: "/logos/zepto.png",
-       price: 131,
-       time: 12,
-     },
-     {
-       name: "Instamart",
-       logo: "/logos/instamart.png",
-       price: 168,
-       time: 14,
-     },
-   ],
- },
- {
-   name: "milk",
-   results: [
-     {
-       name: "Blinkit",
-       logo: "/logos/blinkit.png",
-       price: 73,
-       time: 12,
-     },
-     {
-       name: "Zepto",
-       logo: "/logos/zepto.png",
-       price: 75,
-       time: 12,
-     },
-     {
-       name: "Instamart",
-       logo: "/logos/instamart.png",
-       price: 72,
-       time: 14,
-     },
-   ],
- },
- {
-   name: "potato",
-   results: [
-     {
-       name: "Blinkit",
-       logo: "/logos/blinkit.png",
-       price: 42,
-       time: 10,
-     },
-     {
-       name: "Zepto",
-       logo: "/logos/zepto.png",
-       price: 45,
-       time: 11,
-     },
-     {
-       name: "Instamart",
-       logo: "/logos/instamart.png",
-       price: 40,
-       time: 15,
-     },
-   ],
- },
+ {
+   name: 'almond',
+   results: [
+     {
+       name: 'Blinkit',
+       price: 711,
+       time: 12
+     },
+     {
+       name: 'Zepto',
+       price: 718,
+       time: 13
+     },
+     {
+       name: 'Instamart',
+       price: 705,
+       time: 19
+     }
+   ]
+ },
+
+ {
+   name: 'apple',
+   results: [
+     {
+       name: 'Blinkit',
+       price: 130,
+       time: 12
+     },
+     {
+       name: 'Zepto',
+       price: 131,
+       time: 12
+     },
+     {
+       name: 'Instamart',
+       price: 168,
+       time: 14
+     }
+   ]
+ },
+
+ {
+   name: 'milk',
+   results: [
+     {
+       name: 'Blinkit',
+       price: 73,
+       time: 12
+     },
+     {
+       name: 'Zepto',
+       price: 75,
+       time: 12
+     },
+     {
+       name: 'Instamart',
+       price: 72,
+       time: 14
+     }
+   ]
+ },
+
+ {
+   name: 'potato',
+   results: [
+     {
+       name: 'Blinkit',
+       price: 42,
+       time: 10
+     },
+     {
+       name: 'Zepto',
+       price: 45,
+       time: 11
+     },
+     {
+       name: 'Instamart',
+       price: 40,
+       time: 15
+     }
+   ]
+ }
 ]
